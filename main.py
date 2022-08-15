fcb139f214ee6a1db8aea5d87b7ea2e0b6541604
Showtimeeeeexitgit config --global user.email sipovichit@yandex.rugit config --global user.name Showtimeeee <sipovichit@yandex.ru>
2022-08-15 15:50:12 +0300

    new button, comments

--- main.py
+++ main.py
@@ -3,20 +3,27 @@ import matplotlib
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 
-
+# func graph
 def update_par(data):
+    # оси
     axes = fig.axes
+    # x = first el
     x = [i[0] for i in data]
+    # int for graph accuracy
     y = [int(i[1]) for i in data]
-    axes[0].plot(x, y, 'b-')
+    # points connected by green lines
+    axes[0].plot(x, y, 'g-')
     figure_canvas_agg.draw()
     figure_canvas_agg.get_tk_widget().pack()
 
 
+# beautiful theme
+sg.theme("DarkTeal2")
 
-
-sg.theme("LightBlue")
+# counter
 table_content = []
+
+# create layout and add 2 header arguments, resize window, scroll, table
 maket = [
     [sg.Table(
         headings=['Ось', 'Область'],
@@ -25,15 +32,17 @@ maket = [
         hide_vertical_scroll=True,
         key='-TABLE-')],
     [sg.Input(key='-INPUT-', expand_x=True), sg.Button('Принять')],
+    [sg.Canvas(expand_x=True), sg.Button('Удалить')],
     [sg.Canvas(key='-CANVAS-')]
 ]
 
-# gui + graph
+# gui + graph, finalize to edit
 window = sg.Window('Quick Graph', maket, finalize=True)
 
 # matplotlib
 fig = matplotlib.figure.Figure(figsize=(5, 5))
-fig.add_subplot(111).plot([], [])
+# draw plot in CANVAS, 111 = 1×1 grid
+fig.add_subplot(1, 1, 1).plot([], [])
 figure_canvas_agg = FigureCanvasTkAgg(fig, window['-CANVAS-'].TKCanvas)
 figure_canvas_agg.draw()
 figure_canvas_agg.get_tk_widget().pack()
@@ -44,13 +53,23 @@ while True:
     if event == sg.WIN_CLOSED:
         break
 
+    # counter
     if event == 'Принять':
         new_value = values['-INPUT-']
+        # check if the type is numeric, append [] + 1
         if new_value.isnumeric():
             table_content.append([len(table_content) + 1, float(new_value)])
             window['-TABLE-'].update(table_content)
-            window['-INPUT-'].update('')
+            # func
             update_par(table_content)
+            # clear input
+            window['-INPUT-'].update('')
+
+    # clear table
+    if event == 'Удалить':
+        window['-TABLE-'].update([])
+        window['-INPUT-'].update('')
+
 
 window.close()
 
